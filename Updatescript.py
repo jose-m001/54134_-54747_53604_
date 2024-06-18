32aeb110daee9679a8b925e085dcdcea0676b393
Jose <110602365+jose-m001@users.noreply.github.com>
2024-06-18 16:01:14 +0200

    Update Updatescript.py

--- Updatescript.py
+++ Updatescript.py
@@ -8,6 +8,7 @@ import os
 
 time_now = datetime.now().strftime("%Y-%m-%d_%I-%M-%S_%p")
 
+
 folder_to_save_files = 'script_output'
 file_base = "output_" + time_now
 file_extension = ".txt"
@@ -26,6 +27,7 @@ f3_file_base = f3_folder / file_base
 f3_file = f3_file_base.with_suffix(file_extension)
 
 with f3_file.open('w') as file3:
+    
     for device in file1:
         device_params = {
             'device_type': 'cisco_ios',
@@ -43,4 +45,5 @@ with f3_file.open('w') as file3:
             print(f'\n *** Sending {cmd} *** \n')
             print(f'{output} \n')
             file3.write(f'\n *** Sending {cmd} *** \n')
-            file3.write(f'{output} \n')
+            file3.write(f' {output} \n')
+        file3.close
